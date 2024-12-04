339a9d3a98a0f1c8baea761c77d9c9c2002dad1a
Jenni4B <jennifer.ea.blocker@gmail.com>
2024-12-04 10:26:26 -0500

    Initial commit

---
+++ utils.js
@@ -0,0 +1,22 @@
+// Instructions:
+// Implement the following functions using ES6 features:
+// fetchData(url) - Fetches data from a given API using async/await
+
+
+
+// adds a+b and returns the sum of the values passed
+const add = (a, b) => a + b;
+
+// // takes a string and capitalize the first letter of the value passed to the function
+// const capitalize = (str) => `${str.charAt(0).toUpperCase()}${str.slice(1)}`;
+
+// // fetch API
+
+// const fetchData = async (url) => {
+
+
+// }
+
+
+
+console.log(add(62, 6))
